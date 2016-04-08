f14d8c1964dd433f842df820db3ceacc978c583f
eero kauppi <eero.kauppi@gmail.com>
2016-04-08 13:34:38 +0300

    Lisätty TypeScript-tuki projektiin ja samalla aloitettu JSON-testisivun toteutus.

---
+++ AssetManagementWeb/AssetManagementWeb/Scripts/Application/Logic.js
@@ -0,0 +1,7 @@
+/// <reference path="../typings/jquery/jquery.d.ts" />
+function initAssetAssigment() {
+    $("#AssignAssetButton").click(function () {
+        alert("Toimii!");
+    });
+}
+//# sourceMappingURL=Logic.js.map
